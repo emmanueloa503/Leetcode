4b7fb4e183d41add9f37f3f1032dd2571835261e
Emmanuel Owusu-Ampaw <118029142+emmanueloa503@users.noreply.github.com>
2025-04-24 16:31:40 -0400

    Add files via upload

---
+++ week 8/Partition_Equal_Subset_Sum.py
@@ -0,0 +1,19 @@
+class Solution(object):
+    def canPartition(self, nums):
+        """
+        :type nums: List[int]
+        :rtype: bool
+        """
+        total_sum = sum(nums)
+        if total_sum % 2 != 0:
+            return False  
+        
+        target = total_sum // 2
+        dp = [False] * (target + 1)
+        dp[0] = True  
+
+        for num in nums:
+            for i in range(target, num - 1, -1):
+                dp[i] = dp[i] or dp[i - num]
+        
+        return dp[target]        
